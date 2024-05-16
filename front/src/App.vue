c8bb81c251867160c89a8e6e8dbd159e7f58049e
HannahKang <hanna0175@gmail.com>
2024-05-17 00:57:38 +0900

    🎉 feat : front 초기 프로젝트 세팅
    - 불필요한 파일, 코드 삭제
    - 효율적이고 일관된 css 스타일을 위한 tailwind 적용
    - 상태관리를 위한 pinia 세팅
    - 코드 일관성 유지 등을 위한 eslint, prettier and husky 적용

---
+++ front/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <HomeView />
+  </div>
+</template>
+
+<script setup>
+import HomeView from '@/views/HomeView.vue';
+</script>
---
+++ front/src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1 class="text-blue-600">Main</h1>
+  </div>
+</template>
+
+<script setup></script>
