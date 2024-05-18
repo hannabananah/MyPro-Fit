42fa1eb9ad13fadc15e6c3595882c22868b1d0ff
김민채 <alsco9707@gmail.com>
2024-05-18 11:15:00 +0900

    ✅ add : 특정국가의 환율 뽑아내는 computed

--- front/src/stores/exchange.js
+++ front/src/stores/exchange.js
@@ -8,13 +8,14 @@ export const useExchangeStore = defineStore(
     const today = ref([]);
     const lastWeek = ref([]);
     const lastMonth = ref([]);
+
     const fetchExchangeRate = function () {
       axios({
         url: 'http://127.0.0.1:8000/exchange-rate/api/v1/today/',
         method: 'get',
       })
         .then(res => {
-          console.log(res.data);
+          // console.log(res.data);
           today.value = res.data;
         })
         .catch(err => {
@@ -25,7 +26,7 @@ export const useExchangeStore = defineStore(
         method: 'get',
       })
         .then(res => {
-          console.log(res.data);
+          // console.log(res.data);
           lastWeek.value = res.data;
         })
         .catch(err => {
@@ -36,22 +37,16 @@ export const useExchangeStore = defineStore(
         method: 'get',
       })
         .then(res => {
-          console.log(res.data);
+          // console.log(res.data);
           lastMonth.value = res.data;
         })
         .catch(err => {
           console.log(err);
         });
+      console.log(today.value);
     };
 
-    // 특정시간의 특정국가의 환율 객체를 뽑는다
-    const getCountryEx = function (time, country) {
-      console.log(time.value);
-      // const obj = time.value.filter(exc => exc.cur_unit === country);
-      // console.log('선택한 국가의 환율: ', obj);
-    };
-
-    return { fetchExchangeRate, lastWeek, today, lastMonth, getCountryEx };
+    return { fetchExchangeRate, lastWeek, today, lastMonth };
   },
   { persist: true },
 );
--- front/src/views/ExchangeView.vue
+++ front/src/views/ExchangeView.vue
@@ -1,33 +1,36 @@
 <template>
   <div>
     <h1>환율계산기 페이지</h1>
-    <form @submit.prevent="getToday">
+    <form>
+      <select id="standard" v-model="selectedStd">
+        <option value="deal_bas_r">매매기준율</option>
+        <option value="tts">보내실때</option>
+        <option value="ttb">받으실때</option>
+      </select>
+    </form>
+    <form>
       <label for="cur">송금할 외화 : </label>
       <select name="exchange" id="cur" v-model="selectedCur">
-        <option value="AED">아랍에미리트 디르함</option>
-        <option value="AUD">호주 달러</option>
-        <option value="BHD">바레인 디나르</option>
-        <option value="BND">브루나이 달러</option>
-        <option value="CAD">캐나다 달러</option>
-        <option value="CHF">스위스 프랑</option>
-        <option value="CNH">위안화</option>
-        <option value="EUR">유로</option>
-        <option value="GBP">영국 파운드</option>
-        <option value="HKD">홍콩 달러</option>
-        <option value="IDR(100)">인도네시아 루피아</option>
-        <option value="JPY(100)">일본 옌</option>
-        <option value="KRW">한국 원</option>
-        <option value="KWD">쿠웨이트 디나르</option>
-        <option value="MYR">말레이지아 링기트</option>
-        <option value="NOK">노르웨이 크로네</option>
-        <option value="NZD">뉴질랜드 달러</option>
-        <option value="SAR">사우디 리얄</option>
-        <option value="SEK">스웨덴 크로나</option>
-        <option value="SGD">싱가포르 달러</option>
-        <option value="THB">태국 바트</option>
-        <option value="USD">미국 달러</option>
+        <option disabled value="">외화 선택</option>
+        <option
+          v-for="cur in store.today"
+          :key="cur.cur_unit"
+          :value="cur.cur_unit"
+        >
+          {{ cur.cur_unit }} {{ cur.cur_nm }}
+        </option>
       </select>
-      <input type="submit" value="Submit" />
+    </form>
+    <form>
+      <input
+        type="number"
+        placeholder="외화"
+        v-model.number="foreignAmount"
+        @input="convertTokwr"
+      />
+      <input type="number" placeholder="원화" id="kor" /><label for="kor"
+        >원</label
+      >
     </form>
     <hr />
     <ExchangeList />
@@ -35,32 +38,50 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { useExchangeStore } from '@/stores/exchange';
 import ExchangeList from '@/components/ExchangeList.vue';
 
 const store = useExchangeStore();
+const selectedCur = ref(null); // 선택한 통화 저장
+const selectedStd = ref('deal_bas_r'); // 선택한 기준 저장
+const foreignAmount = ref(0); // 외화 입출력값
 
 // 나중에 해당 코드가 서버 켜질때 자동으로 호출되게 바꿔야함
 store.fetchExchangeRate();
 
-// 선택한 통화 저장
-const selectedCur = ref(null);
+// 해당 국가의 환율 계산
+const thisCountryRate = computed(() => {
+  const result = {
+    today: null,
+    lastWeek: null,
+    lastMonth: null,
+  };
 
-// 오늘 환율 불러오는 함수
-const getToday = function () {
-  console.log(store.today.filter(obj => obj.cur_unit === selectedCur.value));
-};
-// 지난주 환율 불러오는 함수
-const getLastWeek = function () {
-  console.log(store.lastWeek.filter(obj => obj.cur_unit === selectedCur.value));
-};
-// 저번 달 환율 불러오는 함수
-const getLastMonth = function () {
-  console.log(
-    store.lastMonth.filter(obj => obj.cur_unit === selectedCur.value),
-  );
-};
+  if (selectedCur.value && selectedStd.value) {
+    const filteredToday = store.today.filter(
+      obj => obj.cur_unit === selectedCur.value,
+    );
+    const filteredLastWeek = store.lastWeek.filter(
+      obj => obj.cur_unit === selectedCur.value,
+    );
+    const filteredLastMonth = store.lastMonth.filter(
+      obj => obj.cur_unit === selectedCur.value,
+    );
+
+    if (filteredToday.length > 0) {
+      result.today = filteredToday[0][selectedStd.value];
+    }
+    if (filteredLastWeek.length > 0) {
+      result.lastWeek = filteredLastWeek[0][selectedStd.value];
+    }
+    if (filteredLastMonth.length > 0) {
+      result.lastMonth = filteredLastMonth[0][selectedStd.value];
+    }
+  }
+
+  return result;
+});
 </script>
 
 <style scoped></style>
