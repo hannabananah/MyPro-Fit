ac30d0126881d24fbaa97efa2c41080d7059ed28
HannahKang <hanna0175@gmail.com>
2024-05-19 13:52:35 +0900

    ✨ feat : django-로그인 기능구현

--- back/accounts/models.py
+++ back/accounts/models.py
@@ -1,5 +1,19 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+from .manages import CustomUserManager
 
 
-class User(AbstractUser):
-    pass
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=6, blank=False, default='이름')
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+
+    def __str__(self):
+        return self.email
