f5a5e9a1def9cb2b22385fb698d30e274e8cf6c8
HannahKang <hanna0175@gmail.com>
2024-05-21 04:28:52 +0900

    ✨ feat : 프로필 페이지 라우터, 뷰 생성

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -4,6 +4,7 @@ import LoginView from '@/views/LoginView.vue';
 import SignupView from '@/views/SignupView.vue';
 import ExchangeView from '@/views/ExchangeView.vue';
 import MapView from '@/views/MapView.vue';
+import ProfileView from '@/views/ProfileView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,11 @@ const router = createRouter({
       path: '/around-bank',
       name: 'map',
       component: MapView,
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView,
     },
   ],
 });
