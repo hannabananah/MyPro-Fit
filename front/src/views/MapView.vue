d8d69e00cdb439c6fce1ef840256f0f798a4ce85
k1minchae <alsco9707@gmail.com>
2024-05-20 09:38:30 +0900

    💄 style : 지도 페이지 style 추가

--- front/src/views/MapView.vue
+++ front/src/views/MapView.vue
@@ -9,8 +9,35 @@
       class="container mx-auto w-4/5 flex flex-col"
     >
       <img style="width: 100%" src="@/assets/images/map.svg" alt="" />
-      <div id="map-container" style="width: 60%; height: 500px" class="my-8">
-        <div ref="mapContainer" style="width: 100%; height: 100%"></div>
+      <div></div>
+      <div class="flex flex-row mb-4">
+        <div
+          id="map-container"
+          style="width: 50%; height: 400px"
+          class="flex flex-col items-center"
+        >
+          <form class="flex justify-center w-full">
+            <div
+              class="flex justify-between border border-slate-400 rounded-2xl mb-4 w-1/2 h-[5vh] mt-6"
+            >
+              <input
+                type="text"
+                class="bg-transparent w-5/6 ml-5"
+                placeholder="은행 찾기 : 지역명을 입력하세요."
+              />
+              <button>
+                <img
+                  src="@/assets/icons/search-icon-slate200.svg"
+                  class="h-3/5 mr-4"
+                />
+              </button>
+            </div>
+          </form>
+          <div ref="mapContainer" style="width: 100%; height: 80%"></div>
+        </div>
+        <div id="bank-list" class="mt-6 ml-5">
+          <p class="text-xl text-gray-900">목록 보기</p>
+        </div>
       </div>
     </div>
   </div>
