b9a4cade719348750ba2207eaf695540a3ce9daf
k1minchae <alsco9707@gmail.com>
2024-05-20 17:48:13 +0900

    🚚 chore : product 시리얼라이저 임시작성

--- back/products/serializers.py
+++ back/products/serializers.py
@@ -1,4 +1,14 @@
 from rest_framework import serializers
 from .models import Product, ProductOption
 
-# class ProductList()
+class ProductOptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductOption
+        fields = ['month_6', 'month_12', 'month_24', 'month_36']
+
+class ProductSerializer(serializers.ModelSerializer):
+    product_option = ProductOptionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'price', 'product_option']
