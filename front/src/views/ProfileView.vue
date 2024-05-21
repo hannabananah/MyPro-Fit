f630bc8d039c5f5e55a7fb47778724106c8bb7c8
HannahKang <hanna0175@gmail.com>
2024-05-21 14:55:12 +0900

    💄 style : 프로필 페이지, 추가 정보 입력 패널 UI 구현

--- front/src/views/ProfileView.vue
+++ front/src/views/ProfileView.vue
@@ -53,7 +53,8 @@
               <small
                 class="font-light leading-relaxed tracking-wide text-gray-900"
               >
-                회원가입 시에 입력한 정보를 확인하거나 수정할 수 있습니다.
+                회원가입 시에 입력받은 아이디와 닉네임에 대한 정보를 확인할 수
+                있습니다.
               </small>
             </div>
             <form
@@ -106,29 +107,225 @@
             </form>
           </div>
           <div
-            class="flex flex-col gap-4"
+            class="flex flex-col gap-10"
             v-show="activeTab === 'moreInfo'"
             id="moreInfo"
             role="tabpanel"
           >
             <div>
               <h2 class="mb-2 text-lg font-bold text-gray-900">추가 정보</h2>
-              <small
-                class="font-light leading-relaxed tracking-wide text-gray-900"
+              <p
+                class="text-sm font-light leading-relaxed tracking-wide text-gray-900"
               >
-                내게 가장 적합한 금융 상품을 추천받기 위한 추가 정보를
-                입력해주세요.
-              </small>
+                예적금과 연금 상품을 추천 서비스를 이용하기 위해 필요한 추가적인
+                정보입니다.
+                <br />
+                <small class="tracking-tighter text-gray-600">
+                  입력하신 정보는 상품 추천 서비스에만 사용되며 해당 목적 이외의
+                  다른 어떠한 용도로도 사용되지 않습니다.
+                </small>
+              </p>
             </div>
+            <form
+              class="flex flex-col h-full gap-y-4"
+              @submit.prevent="handleSubmit"
+            >
+              <label for="gender">성별</label>
+              <div class="flex space-x-4">
+                <label
+                  v-for="(option, index) in genderOptions"
+                  :key="index"
+                  class="flex items-center justify-center w-10 p-2 border cursor-pointer rounded-xl"
+                  :class="{
+                    [option.bgColor]: userStore.gender === option.value,
+                    [option.defaultBgColor]: userStore.gender !== option.value,
+                  }"
+                  @click="userStore.gender = option.value"
+                >
+                  <component
+                    :is="option.icon"
+                    :fillColor="
+                      userStore.gender === option.value
+                        ? '#fff'
+                        : option.defaultIconColor
+                    "
+                    :size="24"
+                  />
+                  <input
+                    type="radio"
+                    :id="option.value"
+                    :value="option.value"
+                    v-model="userStore.gender"
+                    class="hidden form-radio"
+                  />
+                </label>
+                <label
+                  class="flex items-center justify-center w-10 p-2 border cursor-pointer rounded-xl"
+                  :class="{
+                    'bg-white': userStore.gender === 'female',
+                    'bg-custom-pink': userStore.gender !== 'female',
+                  }"
+                  @click="userStore.gender = 'female'"
+                >
+                  <Female
+                    :fillColor="
+                      userStore.gender === 'female' ? '#fff' : '#F8ACFF'
+                    "
+                    :size="24"
+                  />
+                  <input
+                    type="radio"
+                    id="female"
+                    value="female"
+                    v-model="userStore.gender"
+                    class="hidden form-radio"
+                  />
+                </label>
+                <label
+                  class="flex items-center justify-center w-10 p-2 border cursor-pointer rounded-xl"
+                  :class="{
+                    'bg-custom-blue shadow-inner': userStore.gender === 'male',
+                    'bg-slate-50': userStore.gender !== 'male',
+                  }"
+                  @click="userStore.gender = 'male'"
+                >
+                  <Male
+                    :fillColor="
+                      userStore.gender === 'male' ? '#fff' : '#7DD3FC'
+                    "
+                    :size="24"
+                  />
+                  <input
+                    type="radio"
+                    id="male"
+                    value="male"
+                    v-model="userStore.gender"
+                    class="hidden form-radio"
+                  />
+                </label>
+              </div>
 
-            <div class=""></div>
+              <label for="age">나이</label>
+
+              <div class="relative h-14">
+                <input
+                  type="number"
+                  id="age"
+                  class="w-20 px-6 -mt-2 text-right text-input box-sizing"
+                  :value="userStore.age || 29"
+                  placeholder="29"
+                />
+                <span class="absolute text-sm left-14 top-1">세</span>
+              </div>
+              <div class="flex flex-col w-4/5">
+                <div
+                  class="flex justify-between w-full mt-4"
+                  v-for="option in checkboxOptions"
+                  :key="option.id"
+                >
+                  <input
+                    type="checkbox"
+                    :name="option.id"
+                    :id="option.id"
+                    class="sr-only peer"
+                    v-model="userStore[option.model]"
+                  />
+                  <label
+                    :for="option.id"
+                    class="flex items-center justify-between w-full font-bold cursor-pointer"
+                  >
+                    <span>{{ option.label }}</span>
+                    <div class="w-6 h-6 mr-2">
+                      <CheckboxBlank
+                        v-if="!userStore[option.model]"
+                        class="text-gray-600"
+                      />
+                      <Checkbox v-else class="text-gray-900" />
+                    </div>
+                  </label>
+                </div>
+
+                <div class="flex flex-row-reverse justify-between w-full">
+                  <input
+                    type="checkbox"
+                    name="pension"
+                    id="pension"
+                    class="sr-only peer"
+                    v-model="userStore.is_pension"
+                  />
+                  <label for="pension" class="flex items-center cursor-pointer">
+                    <CheckboxBlank
+                      class="w-6 h-6 mr-2 text-gray-600 peer-checked:text-gray-900 peer-checked:hidden"
+                    />
+                    <Checkbox
+                      class="w-6 h-6 mr-2 text-gray-600 peer-checked:text-gray-900 peer-checked:block"
+                    />
+                    <span>연금 저축 상품을 추천받으시겠습니까?</span>
+                  </label>
+                </div>
+                <div class="flex flex-row-reverse justify-between w-full">
+                  <input
+                    type="checkbox"
+                    name="pension"
+                    id="pension"
+                    class="sr-only peer"
+                    v-model="userStore.is_pension"
+                  />
+                  <label for="pension" class="flex items-center cursor-pointer">
+                    <CheckboxBlank
+                      class="w-6 h-6 mr-2 text-gray-600 peer-checked:text-gray-900 peer-checked:hidden"
+                    />
+                    <Checkbox
+                      class="w-6 h-6 mr-2 text-gray-600 peer-checked:text-gray-900 peer-checked:block"
+                    />
+                    <span>온라인 가입가능한 상품을 추천받으시겠습니까?</span>
+                  </label>
+                </div>
+                <div class="flex flex-row-reverse justify-between w-full">
+                  <input
+                    type="checkbox"
+                    name="pension"
+                    id="pension"
+                    class="sr-only peer"
+                    v-model="userStore.is_pension"
+                  />
+                  <label for="pension" class="flex cursor-pointer">
+                    <CheckboxBlank
+                      class="w-6 h-6 mr-2 text-gray-600 peer-checked:text-gray-900 peer-checked:hidden"
+                    />
+                    <Checkbox
+                      class="w-6 h-6 mr-2 text-gray-600 peer-checked:text-gray-900 peer-checked:block"
+                    />
+                    <div class="flex flex-col">
+                      <span>기초생활수급자이십니까?</span>
+                      <small class="text-[0 75rem] font-light"
+                        >*국민기초생활보장제도를 통해 지원받을 수 있는 상품을
+                        추천해드리기 위한 질문입니다.</small
+                      >
+                    </div>
+                  </label>
+                </div>
+                <div class="flex flex-row-reverse justify-between w-full">
+                  <input
+                    type="checkbox"
+                    name="pension"
+                    id="pension"
+                    class="sr-only peer"
+                    v-model="userStore.is_pension"
+                  />
+                  <label for="pension" class="flex items-center cursor-pointer">
+                    <CheckboxBlank
+                      class="w-6 h-6 mr-2 text-gray-600 peer-checked:text-gray-900 peer-checked:hidden"
+                    />
+                    <Checkbox
+                      class="w-6 h-6 mr-2 text-gray-600 peer-checked:text-gray-900 peer-checked:block"
+                    />
+                    <span>자유납입상품을 추천받으시겠습니까?</span>
+                  </label>
+                </div>
+              </div>
+            </form>
           </div>
-          <!--      <h2 class="mb-2 text-lg font-bold text-gray-900">Profile Tab</h2>
-          <p class="mb-2">
-            This is some placeholder content the Profile tab's associated
-            content, clicking another tab will toggle the visibility of this one
-            for the next.
-          </p> -->
         </div>
         <div class="flex w-1/2 md:w-full">
           <button
@@ -166,11 +363,16 @@ import Profile from 'vue-material-design-icons/AccountCog.vue';
 import Wallet from 'vue-material-design-icons/WalletPlus.vue';
 import Email from 'vue-material-design-icons/EmailOutline.vue';
 import Account from 'vue-material-design-icons/AccountOutline.vue';
+import Female from 'vue-material-design-icons/GenderFemale.vue';
+import Male from 'vue-material-design-icons/GenderMale.vue';
+import CheckboxBlank from 'vue-material-design-icons/CheckboxBlankOutline.vue';
+import Checkbox from 'vue-material-design-icons/CheckboxOutline.vue';
 import axios from 'axios';
 import CustomModal from '@/components/Modal.vue';
 import { useUserStore } from '@/stores/user';
 const userStore = useUserStore();
-const activeTab = ref('profile');
+// const activeTab = ref('profile');
+const activeTab = ref('moreInfo');
 const isModalOpen = ref(false);
 
 onMounted(() => {
@@ -181,10 +383,14 @@ watch(
   () => ({
     username: userStore.username,
     nickname: userStore.nickname,
+    gender: userStore.gender,
+    age: userStore.age,
   }),
   newValues => {
     userStore.username = newValues.username;
     userStore.nickname = newValues.nickname;
+    userStore.gender = newValues.gender;
+    userStore.age = newValues.age;
   },
   { immediate: true },
 );
@@ -198,6 +404,7 @@ const updateProfile = () => {
     console.error('Token is not set');
     return;
   }
+
   axios({
     method: 'patch',
     url: `${userStore.API_URL}/accounts/user/`,
@@ -217,9 +424,40 @@ const updateProfile = () => {
     });
 };
 
+const updateMoreInfo = () => {
+  const genderMap = {
+    female: 'F',
+    male: 'M',
+  };
+  const genderValue = genderMap[userStore.gender];
+
+  if (!userStore.token) {
+    console.error('Token is not set');
+    return;
+  }
+  axios({
+    method: 'patch',
+    url: `${userStore.API_URL}/accounts/user/`,
+    data: {
+      gender: genderValue,
+      age: userStore.age,
+      is_pension: userStore.is_pension,
+      is_internet: userStore.is_internet,
+      is_BLSR: userStore.is_BLSR,
+      is_free: userStore.is_free,
+    },
+  })
+    .then(response => {
+      console.log('More info 업데이트 성공', response);
+    })
+    .catch(error => {
+      console.error('More info 업데이트 실패', error);
+    });
+};
+
 const handleConfirm = () => {
   isModalOpen.value = false;
-  updateProfile();
+  activeTab.value === 'profile' ? updateProfile() : updateMoreInfo();
 };
 
 const handleCancel = () => {
