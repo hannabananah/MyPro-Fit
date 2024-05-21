70566e6ed432b5994e9c8a61d0277096e0b08536
HannahKang <hanna0175@gmail.com>
2024-05-21 14:54:42 +0900

    🚚 fix : isLogin 함수 불러오는 경로 수정

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <RouterLink v-if="!isLogin" :to="{ name: 'login' }" class="text-orange-600"
+    <RouterLink
+      v-if="!userStore.isLogin"
+      :to="{ name: 'login' }"
+      class="text-orange-600"
       >Login</RouterLink
     >
-    <button v-if="isLogin" @click="logOut">Log out</button>
+    <button v-if="userStore.isLogin" @click="logOut">Log out</button>
     <h1 class="text-blue-600">Main</h1>
     <p>
-      <strong>{{ nickname }}</strong
+      <strong>{{ userStore.nickname }}</strong
       >님 안녕하세요!
     </p>
-    <button v-if="isLogin" @click="openModal">회원탈퇴</button>
+    <button v-if="userStore.isLogin" @click="openModal">회원탈퇴</button>
   </div>
   <CustomModal
     v-model="isModalOpen"
@@ -31,7 +34,6 @@ import CustomModal from '@/components/Modal.vue';
 import { onMounted } from 'vue';
 
 const userStore = useUserStore();
-const nickname = userStore.nickname;
 const isModalOpen = ref(false);
 
 const logOut = () => {
@@ -59,7 +61,7 @@ const store = useExchangeStore();
 store.fetchExchangeRate();
 
 onMounted(() => {
-  if (isLogin) {
+  if (userStore.isLogin) {
     console.log(userStore.getUserInfo());
     userStore.getUserInfo();
   }
