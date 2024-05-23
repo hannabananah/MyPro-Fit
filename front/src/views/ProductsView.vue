b61895c09f568a956c3b244f5c36f40b1cb32d18
HannahKang <hanna0175@gmail.com>
2024-05-24 02:44:05 +0900

    💄 update : 예적금 금리 비교 페이지 풀페이지 구현

--- front/src/views/ProductsView.vue
+++ front/src/views/ProductsView.vue
@@ -1,45 +1,36 @@
 <template>
-  <div class="py-12">
-    <h1 class="ml-[14%] text-2xl mb-4 font-bold">예적금 금리 비교</h1>
-    <div
-      style="
-        border-radius: 60px;
-        overflow: hidden;
-        box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.05);
-      "
-      class="container mx-auto w-4/5 flex flex-col px-6 border border-solid border-slate-200"
-    >
-      <fieldset class="mt-12 mb-8 text-l">
-        <legend class="my-1">조회하실 상품을 선택하세요.</legend>
-        <router-link
-          :class="{
-            'btn-active': route.name === 'deposit',
-            'btn-inactive': route.name !== 'deposit',
-          }"
-          class="px-4 mr-2"
-          :to="{ name: 'deposit' }"
-          >예금 조회</router-link
-        >
-
-        <router-link
-          :class="{
-            'btn-active': route.name === 'saving',
-            'btn-inactive': route.name !== 'saving',
-          }"
-          class="px-4"
-          :to="{ name: 'saving' }"
-          >적금 조회</router-link
-        >
-
-        <!-- <router-link :to="{ name: 'annuity' }">연금 비교</router-link> -->
-      </fieldset>
-      <RouterView />
-    </div>
+  <h1 class="ml-[14%] text-2xl mb-3 font-bold -mt-1">예적금 금리 비교</h1>
+  <div
+    class="flex flex-col w-4/5 px-6 mx-auto overflow-y-auto border shadow-lg rounded-3xl border-slate-200 border-box"
+  >
+    <fieldset class="h-full my-2 text-lg">
+      <legend class="my-1">조회하실 상품을 선택하세요.</legend>
+      <router-link
+        :class="{
+          'btn-active': route.name === 'deposit',
+          'btn-inactive': route.name !== 'deposit',
+        }"
+        class="px-4 mr-2"
+        :to="{ name: 'deposit' }"
+        >예금 조회</router-link
+      >
+      <router-link
+        :class="{
+          'btn-active': route.name === 'saving',
+          'btn-inactive': route.name !== 'saving',
+        }"
+        class="px-4"
+        :to="{ name: 'saving' }"
+        >적금 조회</router-link
+      >
+      <!-- <router-link :to="{ name: 'annuity' }">연금 비교</router-link> -->
+    </fieldset>
+    <RouterView />
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { onMounted } from 'vue';
 import { RouterLink, RouterView } from 'vue-router';
 import { useRoute } from 'vue-router';
 import { useProductStore } from '@/stores/products';
@@ -51,5 +42,3 @@ onMounted(() => {
   store.fetchSaving();
 });
 </script>
-
-<style scoped></style>
