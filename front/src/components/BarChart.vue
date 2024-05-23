6315a5ddcb3d453d02ea975a6e0ddae6f537063a
k1minchae <alsco9707@gmail.com>
2024-05-23 16:21:03 +0900

    🐛 fix : 차트 수정

--- front/src/components/BarChart.vue
+++ front/src/components/BarChart.vue
@@ -4,6 +4,7 @@
     id="my-chart-id"
     :options="chartOptions"
     :data="chartData"
+    class="max-w-[40rem]"
   />
 </template>
 <script setup>
@@ -47,6 +48,21 @@ onMounted(() => {
   const products = store.joinedProdudcts.deposit_join_products;
   const colors = ['#7BB4E3', '#90B1DB', '#C5D4EB', '#A3CEEF', '#3E77B6'];
   for (const product of products) {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    const maxVal = Math.max(
+      product.month_6,
+      product.month_12,
+      product.month_24,
+      product.month_36,
+    );
+    chartData.value.labels.push(product.fin_prdt_nm);
+    chartData.value.datasets[0].data.push(maxVal);
+    chartData.value.datasets[0].backgroundColor.push(colors[randomIndex]);
+    chartDataReady.value = true;
+  }
+
+  const productsSaving = store.joinedProdudcts.saving_join_products;
+  for (const product of productsSaving) {
     const randomIndex = Math.floor(Math.random() * colors.length);
     const maxVal = Math.max(
       product.month_6,
