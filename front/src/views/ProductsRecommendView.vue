898f165e5da81e16749d60ebcfa725a74a3fa013
k1minchae <alsco9707@gmail.com>
2024-05-23 16:57:40 +0900

    💄 style : recommendview

--- front/src/views/ProductsRecommendView.vue
+++ front/src/views/ProductsRecommendView.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>
-    <h1>상품 추천 페이지</h1>
-    <div>
-      <p>
-        입력하신 정보를 바탕으로 {{ userStore.nickname }}님에게 Fit 한 금융
-        상품을 추천해드려요 !
+  <div class="py-12">
+    <div
+      style="
+        border-radius: 60px;
+        overflow: hidden;
+        box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.05);
+      "
+      class="flex flex-col items-center w-3/5 mx-auto"
+    >
+      <img src="@/assets/images/bankproducts.png" alt="" />
+
+      <h1 class="-mt-8 text-2xl italic font-bold mb-8">
+        "금융 상품을 추천해드려요 !"
+      </h1>
+
+      <p class="text-l text-center">
+        입력하신 정보를 바탕으로 <br />
+        <span class="font-bold">{{ userStore.nickname }}</span>
+        님에게
+        <span class="font-bold text-sky-700 text-xl italic">Fit</span> 한 금융
+        상품을 추천해드립니다.
       </p>
+      <button class="btn-active w-[50%] my-6" @click="onClick">
+        상품 추천 받기
+      </button>
+      <img class="mb-4" src="@/assets/images/logo.svg" alt="" />
     </div>
-    <button @click="onClick">상품 추천 받기</button>
   </div>
 </template>
 
