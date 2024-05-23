4edee061a0ece9f864ea173efadc0013f8d1879c
HannahKang <hanna0175@gmail.com>
2024-05-23 19:52:33 +0900

    🐛 fix : 병합 충돌로 인한 에러 해결

--- front/src/App.vue
+++ front/src/App.vue
@@ -16,18 +16,11 @@
       :to="{ name: 'profile', params: { tab: 'basicInfo' } }"
       >마이 페이지</RouterLink
     >
-    <RouterLink :to="{ name: 'recommend' }">상품 추천 받기</RouterLink>
+    <div class="relative w-full h-[calc(100vh-114px)]">
+      <RouterView />
+      <ChatbotFloatingBtn class="sticky bottom-0 right-0" />
+    </div>
   </nav>
-  <RouterView />
-  <RouterLink :to="{ name: 'home' }">Home</RouterLink>
-  <RouterLink :to="{ name: 'deposit' }">예적금 금리 비교</RouterLink>
-  <RouterLink :to="{ name: 'exchange' }">환율 계산기</RouterLink>
-  <RouterLink :to="{ name: 'map' }">은행 지점찾기</RouterLink>
-  <RouterLink v-if="isLogin" :to="{ name: 'profile' }">마이 페이지</RouterLink>
-  <div class="relative w-full h-[calc(100vh-114px)]">
-    <RouterView />
-    <ChatbotFloatingBtn class="sticky bottom-0 right-0" />
-  </div>
 </template>
 
 <script setup>
