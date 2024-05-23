91942e89dad6b207b1da571c2e3281b1018a117c
HannahKang <hanna0175@gmail.com>
2024-05-23 14:15:15 +0900

    🌱 add : 챗봇 라우팅 설정

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -14,6 +14,7 @@ import CreateArticle from '@/components/CreateArticle.vue';
 import BoardView from '@/views/BoardView.vue';
 import BoardArticleDetail from '@/components/BoardArticleDetail.vue';
 import UpdateArticle from '@/components/UpdateArticle.vue';
+import ChatbotView from '@/views/ChatbotView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -94,6 +95,11 @@ const router = createRouter({
       path: '/board/:id/update',
       name: 'update-article',
       component: UpdateArticle,
+    },
+    {
+      path: '/chatbot',
+      name: 'chatbot',
+      component: ChatbotView,
     },
   ],
 });
