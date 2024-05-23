f03b2dcdd6dbc33736a3a552f7c7ed28e1dc7989
HannahKang <hanna0175@gmail.com>
2024-05-24 00:47:16 +0900

    ✨ feat : navbar UI 구현

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,32 +1,15 @@
 <template>
-  <nav
-    v-if="
-      $route.name !== 'product-detail' &&
-      $route.name !== 'login' &&
-      $route.name !== 'error' &&
-      $route.name !== 'signup'
-    "
-  >
-    <RouterLink :to="{ name: 'home' }">Home</RouterLink>
-    <RouterLink :to="{ name: 'deposit' }">예적금 금리 비교</RouterLink>
-    <RouterLink :to="{ name: 'exchange' }">환율 계산기</RouterLink>
-    <RouterLink :to="{ name: 'map' }">은행 지점찾기</RouterLink>
-    <RouterLink
-      v-if="isLogin"
-      :to="{ name: 'profile', params: { tab: 'basicInfo' } }"
-      >마이 페이지</RouterLink
-    >
-  </nav>
-  <div class="relative w-full h-[calc(100vh-114px)]">
-    <RouterView />
-    <ChatbotFloatingBtn class="sticky bottom-0 right-0" />
+  <div class="flex flex-col min-h-screen">
+    <Navbar />
+    <div class="relative flex-grow bg-blue-100">
+      <RouterView />
+      <ChatbotFloatingBtn class="fixed bottom-4 right-4" />
+    </div>
   </div>
 </template>
 
 <script setup>
 import ChatbotFloatingBtn from '@/components/ChatbotFloatingBtn.vue';
-import { RouterLink, RouterView } from 'vue-router';
-import { useUserStore } from '@/stores/user';
-const userStore = useUserStore();
-const isLogin = userStore.isLogin;
+import { RouterView } from 'vue-router';
+import Navbar from '@/components/Navbar.vue';
 </script>
